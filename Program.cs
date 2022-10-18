1502b2ce145b73e175f59efbfc242ebd0e00ecf5
Alina <kan.alina2017@yandex.ru>
2022-10-18 23:23:31 +0300

    Запись числе от -n до n

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine()!);
+int M = N * (-1);
+
+if (N<0)
+{
+    int temp = N;
+    N = M;
+    M = temp;
+}
+while(M<=N)
+{
+    Console.WriteLine(M + " ");
+    M++;
+}
